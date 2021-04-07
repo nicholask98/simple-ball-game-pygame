451e84518a0f05dd31339f296f7e974cc4324e84
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-04-06 22:07:17 -0700

    Initial Update. Just commented code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-ball-game-pygame
  
+Based on https://www.teachyourselfpython.com/challenges.php?a=03_Pygame_Challenges_and_Learn&t=02_Class_based_Ball_game&s=00_Intro
+
+Challenge 1:
+1. Copy and paste the code below and run it to ensure it works
+2. See if you can comment the code, explain it, and extend it to do the following: 
+>> Create user key (up, down, left right arrow key) movement for the ball.
---
+++ main.py
@@ -0,0 +1,48 @@
+# gain access to the pygame module
+import pygame
+
+# defines guidelines for all "Ball" objects
+class Ball:
+    # Every time a 'Ball' type object is created it has these attributes and must have an x, y, radius and screen to pass in
+	def __init__(self, x, y, radius, screen):
+        # all passed in attributes become instance-specific
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.screen = screen
+    # draws a blue circle with the instance's radius in the instance's x,y position
+	def draw(self):
+		pygame.draw.circle(screen, (0, 0, 255), [self.x, self.y], self.radius)
+
+# initializes pygame. Required to start using the module.
+pygame.init()
+
+# sets the window at 400px wide by 300 px tall
+screen = pygame.display.set_mode((400, 300))
+
+# sets variable for game loop to false
+done = False
+
+# Not sure exactly what Clock function is. I'm guessing it tracks the amount of time since Pygame was initialized.
+clock = pygame.time.Clock()
+
+# creates a new ball object named "ball1" with position (100, 100), a radius of 20 and on the screen window.
+ball1 = Ball(100, 100, 20, screen)
+
+# begin game loop. Will continue to loop until done == True
+while not done:
+    # loops through event cue checking for Pygame QUIT event. If QUIT is True in any event, done = True and the game loop ends.
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+    # fills window with black
+	screen.fill((0, 0, 0))
+
+    # uses Ball method "draw" to paint the ball in its current position on the screen.
+	ball1.draw()
+
+    # updates the window
+	pygame.display.flip()
+
+    # game loop updates every 60 ms
+	clock.tick(60)
